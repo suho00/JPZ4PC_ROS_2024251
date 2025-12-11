9c13a966015d5c5084e31a7964e4260d2eca5277
ros_user <ros_user@ubuntu-vm.myguest.virtualbox.org>
2025-12-11 16:22:05 +0100

    ros2_course alap projekt feltoltese

---
+++ ros2_course/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'ros2_course'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name),
+            glob('launch/*launch.[pxy][yma]*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros_user',
+    maintainer_email='ros_user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hello = ros2_course.hello:main',
+            'topic_latcher = ros2_course.topic_latcher:main',
+        ],
+    },
+)
